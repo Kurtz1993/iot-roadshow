ab352c37109e458bb14fea158bc94dcb55a19ae0
jrs <jrs@dev.msw.com.mx>
2015-10-22 18:38:39 -0500

    save mongoose

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 var mosca = require('mosca');
+var uriDB = 'mongodb://localhost/IoT'
+var mongoose = require('mongoose').connect(uriDB);
+var Schema = mongoose.Schema;
+
+var publiched = new Schema({
+	topic : String,
+	value : String,
+	stamp : Date	
+});
+var Datos = mongoose.model('Datos', publiched);
+
 var moscaSettings = {
 	port: 8080,
 };
@@ -22,15 +33,21 @@ server.on('clientDisconnected', function (client) {
 
 server.on('published', function (packet, client) {
 	console.log('Published MIO: ', packet.payload.toString());
+	var publish = new Datos({
+		topic: packet.topic.toString(),
+		value: packet.payload.toString(),
+		stamp: Date.now()
+	});
+	publish.save();
 });
 
 server.on('ready', setup);
 
 function setup() {
 	console.log('Mosca is up and running at port 1883');
-	setInterval(function () {
-		server.publish(message, function () {
-			console.log('Published message');
-		});
-	}, 5000);
+	// setInterval(function () {
+	// 	server.publish(message, function () {
+	// 		console.log('Published message');
+	// 	});
+	// }, 5000);
 }
