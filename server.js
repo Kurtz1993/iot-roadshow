aee8244eed3aadbdaab8362042fd73518df9a7e3
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2015-10-22 17:39:45 -0500

    Added Mosca server code.

---
+++ server.js
@@ -0,0 +1,36 @@
+var mosca = require('mosca');
+var moscaSettings = {
+	port: 8080,
+};
+
+var message = {
+	topic: '/test',
+	payload: 'Test message...',
+	qos: 1,
+	retain: false
+};
+
+var server = new mosca.Server(moscaSettings);
+
+server.on('clientConnected', function (client) {
+	console.log('Client connected: ', client.id);
+});
+
+server.on('clientDisconnected', function (client) {
+	console.log('clientDisconnected : ', client.id);
+});
+
+server.on('published', function (packet, client) {
+	console.log('Published MIO: ', packet.payload.toString());
+});
+
+server.on('ready', setup);
+
+function setup() {
+	console.log('Mosca is up and running at port 1883');
+	setInterval(function () {
+		server.publish(message, function () {
+			console.log('Published message');
+		});
+	}, 5000);
+}
